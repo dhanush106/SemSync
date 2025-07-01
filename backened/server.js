720dcb5c1dfd46f98b5b33d339b2999fc7492f8f
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2025-07-01 21:46:37 +0530

    Update server.js

--- backened/server.js
+++ backened/server.js
@@ -30,7 +30,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://semsync.netlify.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
